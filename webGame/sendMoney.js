5608537ffd7a047686c4006a32b1416be58cfbd2
CarlosAlegre <calegreu@gmail.com>
2022-07-25 00:02:27 +0200

    Learning front-end Basics

--- webGame/sendMoney.js
+++ webGame/sendMoney.js
@@ -1,13 +1,15 @@
 import { ethers } from "./ethers-5.6.esm.min.js"
 
+const deployButton = document.getElementById("deployContract")
+deployButton.onclick = connect
+
 async function connect() {
     try {
         if (typeof window.ethereum !== "undefined") {
             console.log("METAMASK DETECTED")
             await window.ethereum.request({ method: "eth_requestAccounts" })
             console.log("Connected")
-            document.getElementById("connectButton").innerHTML =
-                "CONNECTED!!! (from index.js)"
+            document.getElementById("deployContract").innerHTML = "CONNECTED!!! (from index.js)"
         } else {
             alert("METAMASK NOT DETECTED, PLEASE INSTALL IT")
         }
