f651b2d127a54324ab731dfaeffdfaa6bf85904e
CarlosAlegre <calegreu@gmail.com>
2022-12-11 18:51:30 +0100

    Updated paths

--- webGame/constants/index.js
+++
@@ -1,7 +0,0 @@
-const contractsAddresses = require("./contractAddresses.json")
-const contractsAbis = require("./contractsAbis.json")
-
-module.exports = {
-    contractsAddresses,
-    contractsAbis
-}
