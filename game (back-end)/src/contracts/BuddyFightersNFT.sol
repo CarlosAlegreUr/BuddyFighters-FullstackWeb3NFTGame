f64180f7f7e1c160a47ebc35883c0262ce7a8f23
CarlosAlegreUr <CarlosAlegreUr@users.noreply.github.com>
2023-06-07 19:46:50 +0200

    contract, redesign with my own npm package :D

--- game (back-end)/src/contracts/BuddyFightersNFT.sol
+++ game (back-end)/src/contracts/BuddyFightersNFT.sol
@@ -6,53 +6,71 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "interaction-control-contract/InteractionControl.sol";
+
+import "./Fight.sol";
 
 /* Customed erros */
-error BuddyFightersNFT__MinimumPriceNotPayed();
-error BuddyFightersNFT__IsNotFundsManager();
-error BuddyFightersNFT__IsNotTokenOwner();
+error BFNFT__MinimumPriceNotPayed();
+error BFNFT__NotEnoughFunds();
+error BFNFT__FailedToFundFight();
+error BFNFT__RndomNumLengthNotValid();
+error BFNFT__IsNotTokenOwner();
 
 /**
  * @title BuddyFighters' NFTs contract.
  * @author Carlos Alegre Urquizú
  *
- * @notice This contract manages ownership of your nfts from BuddyFighters collection.
- * Only backend addresses owned by the developers team can call some functions of the
- * contract. Though events are emitted so end user can be sure backend does what promises.
- * If our devs betray consumers you can notice by listening if the events are being emitted
- * with the expected values.
- *
- * @dev Only backend addresses by devs team can call some functions of the contract. Though
- * events are emitted so end user can check backend does what promises.
+ * @notice This contract manages your nfts from BuddyFighters collection. It uses
+ * InteractionControl contract to create "agreements" on which NFT's to minst and how
+ * to upgrade them between costumer and the collection devs.
  */
-contract BuddyFightersNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
+contract BuddyFightersNFT is
+    ERC721URIStorage,
+    ERC721Enumerable,
+    Ownable,
+    InteractionControl,
+    VRFConsumerBaseV2
+{
     /* State variables */
     uint256 private constant MINT_PRICE = 10000000000000000;
-    uint256 private constant STATS_CHANGE_PRICE = 10000000000000;
-    uint8 private constant MAX_STATS_VALUE = 254;
-    address private immutable i_independentFundManager;
+    uint256 private constant STATS_CHANGE_PRICE = 10000000000000000;
+    uint256 private constant START_FIGHT_COMMISSION = 10000000000000000;
+    uint8 private constant MAX_PKMN_NUM = 151;
+    uint8 private constant MAX_STATS_VALUE = 255;
+    uint8 private constant STATS_NUM = 6;
+
+    // Chainlink Random numbers generation
+    uint8 private constant BLOCK_CONFIRMATION_FOR_RANDOMNESS = 3;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_vrfSubsId;
+    bytes32 private immutable i_keyHashGasLimit;
+    uint32 private immutable i_callBackGasLimit;
+
+    // Battle bids managing variables.
+    mapping(address => uint256) private s_clientToFunds;
 
     /* Events */
-    event BuddyFightersNFT__NftMinted(
+    event BFNFT__NftMinted(
         address indexed owner,
         uint256 tokenId,
         string tokenURI
     );
-    event BuddyFightersNFT__StatsChanged(
+    event BFNFT__StatsChanged(
         address indexed owner,
         uint256 indexed tokenID,
         string newURI
     );
-    event BuddyFightersNFT__WithdrawalResult(bool indexed success);
-
-    /* Modifiers */
-
-    modifier isFundsManager() {
-        if (msg.sender != i_independentFundManager) {
-            revert BuddyFightersNFT__IsNotFundsManager();
-        }
-        _;
-    }
+    event BFNFT__RndomNumsGenerated(
+        uint8[2] rndmNums,
+        uint256 indexed requestId
+    );
+    event BFNFT__RndomStatsGenerated(
+        uint8[STATS_NUM] rndmNums,
+        uint256 indexed requestId
+    );
+    event BFNFT__FightStarted(address indexed battleAddress);
+    event BFNFT__WithdrawalResult(bool indexed success);
 
     /**
      * Checking if an address is owner of a token.
@@ -62,7 +80,28 @@ contract BuddyFightersNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
      */
     modifier isTokenOwner(uint256 _tokenId, address _tokenOwner) {
         if (ownerOf(_tokenId) != _tokenOwner) {
-            revert BuddyFightersNFT__IsNotTokenOwner();
+            revert BFNFT__IsNotTokenOwner();
+        }
+        _;
+    }
+
+    /**
+     * @dev Checks if client has deposited enough funds before executing the function.
+     */
+    modifier checkEnoughFunds(address _clientAddress, uint256 _price) {
+        if (s_clientToFunds[_clientAddress] < _price) {
+            revert BFNFT__NotEnoughFunds();
+        }
+        _;
+    }
+
+    /**
+     * @dev Sets minimum price in ETH (or blockchain coin) for msg.value
+     * for function to be executed. If not reverts with customed error.
+     */
+    modifier minimumPricePayed(uint256 _price) {
+        if (msg.value < _price) {
+            revert BFNFT__MinimumPriceNotPayed();
         }
         _;
     }
@@ -83,9 +122,15 @@ contract BuddyFightersNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
     constructor(
         string memory _name,
         string memory _symbol,
-        address _independentFundManager
-    ) ERC721(_name, _symbol) {
-        i_independentFundManager = _independentFundManager;
+        address _coordinatorAddress,
+        uint64 _vrfSubsId,
+        bytes32 _keyHashGasLimit,
+        uint32 _callBackGasLimit
+    ) ERC721(_name, _symbol) VRFConsumerBaseV2(_coordinatorAddress) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(_coordinatorAddress);
+        i_vrfSubsId = _vrfSubsId;
+        i_keyHashGasLimit = _keyHashGasLimit;
+        i_callBackGasLimit = _callBackGasLimit;
     }
 
     /**
@@ -96,7 +141,9 @@ contract BuddyFightersNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
      * @notice If you wanna thank the devs team you can call this function
      * to send us a tip. (:D)
      */
-    receive() external payable {}
+    receive() external payable {
+        s_clientToFunds[address(this)] += msg.value;
+    }
 
     /**
      * Someone might be sending us money. (:D)
@@ -106,55 +153,112 @@ contract BuddyFightersNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
      * @notice If you wanna thank the devs team you can call this function with
      * some coin from the blockchain to send us a tip. (:D)
      */
-    fallback() external payable {}
+    fallback() external payable {
+        s_clientToFunds[address(this)] += msg.value;
+    }
 
     /* External functions */
 
     /**
-     * Mints new token.
+     * @dev Mints new token calling _safeMint(), new token's ID is totalSupply() and sets tokenURI with
+     * _setTokenURI(). BaseURI is empty.
      *
-     * @dev Calls _safeMint(), new token's ID is totalSupply() and sets tokenURI with
-     * _setTokenURI(). BaseURI is empty. Warning! Backend must make sure arguments have the correct values.
-     *
-     * @notice Emits an event so clients can see backend called this function for them.
+     * @notice Client must call this function but first the interaction must me allowed by backend.
      */
     function mintNft(
-        string memory _tokenURI,
-        address _clientAddress
-    ) external payable isFundsManager {
+        string memory _tokenURI
+    )
+        external
+        payable
+        minimumPricePayed(MINT_PRICE)
+        isAllowedInteraction(
+            bytes4(keccak256(bytes("mintNft(string)"))),
+            msg.sender,
+            keccak256(abi.encode(_tokenURI))
+        )
+    {
         uint256 tokenId = totalSupply();
-        _safeMint(_clientAddress, tokenId);
+        _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, _tokenURI);
-        emit BuddyFightersNFT__NftMinted(_clientAddress, tokenId, _tokenURI);
+        emit BFNFT__NftMinted(msg.sender, tokenId, _tokenURI);
     }
 
     /**
-     * Changes stats of NFT.
+     * @dev Changes stats of NFT because of change in URI.
      *
-     * @notice Emits an event so client can see backend called this function on his token.
-     *
-     * @dev To change stats backend calls this function which sets the new URI.
-     * Backend should make sure function is called with correct values.
+     * @notice Client must call this function but first the interaction must me allowed by backend.
      *
      * @param _newTokenURI is URI pointing to JSON metadata on IPFS or on-chain
      * metadata in JSON format encoded in base64.
      */
     function changeStats(
         string memory _newTokenURI,
-        address _tokenOwner,
         uint256 _tokenId
-    ) external payable isFundsManager isTokenOwner(_tokenId, _tokenOwner) {
+    )
+        external
+        payable
+        isTokenOwner(_tokenId, msg.sender)
+        minimumPricePayed(STATS_CHANGE_PRICE)
+        isAllowedInteraction(
+            bytes4(keccak256(bytes("changeStats(string,uint256)"))),
+            msg.sender,
+            keccak256(abi.encode(_newTokenURI, _tokenId))
+        )
+    {
         _setTokenURI(_tokenId, _newTokenURI);
-        emit BuddyFightersNFT__StatsChanged(
-            _tokenOwner,
-            _tokenId,
-            _newTokenURI
+        emit BFNFT__StatsChanged(msg.sender, _tokenId, _newTokenURI);
+    }
+
+    function useFundsToStartFight(
+        address[2] calldata _participants,
+        uint256[2] calldata _tokenIds
+    )
+        external
+        payable
+        onlyOwner
+        checkEnoughFunds(_participants[0], START_FIGHT_COMMISSION)
+        checkEnoughFunds(_participants[1], START_FIGHT_COMMISSION)
+    {
+        // Written like this so no there is no error: Stack too deep (:-|)
+        address p1 = _participants[0];
+        address p2 = _participants[1];
+        uint256 tkn1 = _tokenIds[0];
+        uint256 tkn2 = _tokenIds[1];
+
+        Fight fightContract = new Fight(p1, p2, tkn1, tkn2, owner());
+
+        (bool success, ) = payable(fightContract).call{value: msg.value}("");
+
+        if (!success) {
+            revert BFNFT__FailedToFundFight();
+        }
+
+        s_clientToFunds[p1] -= START_FIGHT_COMMISSION;
+        s_clientToFunds[p2] -= START_FIGHT_COMMISSION;
+        emit BFNFT__FightStarted(address(fightContract));
+    }
+
+    /**
+     * @dev Request random numbers to Chainlink network via VRFCoordinator.
+     * Backend calls this function and it requests to the VRFCoordinator to
+     * get random numbers trhough Chainlink network. Number will be recieved
+     * in function:
+     *
+     * `fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override`
+     *
+     * @param _numOfWords quantity of random numbers to generate.
+     */
+    function requestRandomNumbers(uint32 _numOfWords) external onlyOwner {
+        i_vrfCoordinator.requestRandomWords(
+            i_keyHashGasLimit,
+            i_vrfSubsId,
+            BLOCK_CONFIRMATION_FOR_RANDOMNESS,
+            i_callBackGasLimit,
+            _numOfWords
         );
     }
 
     /**
-     * Function to withdraw tips clients give us.
-     *
      * @dev Function to retire funds from contract in case of tips.
      */
     function withdrawContractBalance(
@@ -163,7 +267,88 @@ contract BuddyFightersNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
         (bool success, ) = _accountToSendBalance.call{
             value: address(this).balance
         }("");
-        emit BuddyFightersNFT__WithdrawalResult(success);
+        emit BFNFT__WithdrawalResult(success);
+    }
+
+    /* Public functions */
+
+    /**
+     *
+     * @dev Gets balance of `_address`. Needed to start and deploy fights.
+     *
+     * @param _address is the client's address.
+     */
+    function getBalance(address _address) public view returns (uint256) {
+        return s_clientToFunds[_address];
+    }
+
+    /**
+     * @dev Overriden to mix it with Ownable
+     *
+     * See param specifications in allowfuncCallsFor() docs.
+     */
+    function callAllowInputsFor(
+        address _callerAddress,
+        bytes32[] calldata _validInputs,
+        string calldata _funcSignature,
+        bool _isSequence
+    ) public override onlyOwner {
+        allowInputsFor(
+            _callerAddress,
+            _validInputs,
+            _funcSignature,
+            _isSequence
+        );
+    }
+
+    /**
+     * @dev Overriden to mix it with Ownable
+     *
+     * See param specifications in allowfuncCallsFor() docs.
+     */
+    function callAllowFuncCallsFor(
+        address _callerAddress,
+        bytes4[] calldata _validFuncCalls,
+        bool _isSequence
+    ) public override onlyOwner {
+        allowFuncCallsFor(_callerAddress, _validFuncCalls, _isSequence);
+    }
+
+    /**
+     * @dev Chainlink coordinator returns random numbers requested.
+     *
+     * @notice Emits an event so client can see backend called this function on his token.
+     *
+     * @dev Override comes from VRFConsumerBaseV2. After `requestRandomNumbers(uint32 _numOfWords)`
+     * has been called then i_vrfCoordinator proceeds request and calls this function.
+     *
+     * @param randomWords If pokemon numbers generated then length == 2, if stats generated
+     * then length == 6.
+     *
+     * @param requestId Allows the client to indentify it's request and verify it was indeed proceed.
+     */
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        if (randomWords.length == 2) {
+            uint8 num1 = uint8((randomWords[0] % MAX_PKMN_NUM) + 1);
+            uint8 num2 = uint8((randomWords[1] % MAX_PKMN_NUM) + 1);
+            emit BFNFT__RndomNumsGenerated([num1, num2], requestId);
+        } else {
+            if (randomWords.length == 6) {
+                uint8[6] memory stats;
+                stats[0] = uint8((randomWords[0] % (MAX_STATS_VALUE)) + 1);
+                stats[1] = uint8((randomWords[1] % (MAX_STATS_VALUE)) + 1);
+                stats[2] = uint8((randomWords[2] % (MAX_STATS_VALUE)) + 1);
+                stats[3] = uint8((randomWords[3] % (MAX_STATS_VALUE)) + 1);
+                stats[4] = uint8((randomWords[4] % (MAX_STATS_VALUE)) + 1);
+                stats[5] = uint8((randomWords[5] % (MAX_STATS_VALUE)) + 1);
+                emit BFNFT__RndomStatsGenerated(stats, requestId);
+            } else {
+                revert BFNFT__RndomNumLengthNotValid();
+            }
+        }
     }
 
     /**
@@ -171,22 +356,12 @@ contract BuddyFightersNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
      * inheritance tree.
      */
 
-    /* Public functions */
-
-    /**
-     * @dev This function is here just for solving inheritance ambiguities in
-     * inheritance tree.
-     */
     function supportsInterface(
         bytes4 interfaceId
     ) public view override(ERC721, ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    /**
-     * @dev This function is here just for solving inheritance ambiguities in
-     * inheritance tree.
-     */
     function tokenURI(
         uint256 tokenId
     ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
@@ -195,20 +370,12 @@ contract BuddyFightersNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
 
     /* Internal functions */
 
-    /**
-     * @dev This function is here just for solving inheritance ambiguities in
-     * inheritance tree.
-     */
     function _burn(
         uint256 tokenId
     ) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
-    /**
-     * @dev This function is here just for solving inheritance ambiguities in
-     * inheritance tree.
-     */
     function _beforeTokenTransfer(
         address from,
         address to,
--- game (back-end)/src/services/matchmakingService.js
+++ game (back-end)/src/services/matchmakingService.js
@@ -1,8 +1,9 @@
-const Challenge = require("../databse/models/matchmakingModel");
-const Fight = require("../databse/models/fightModel");
+const Challenge = require("../database/models/matchmakingModel");
+const Fight = require("../database/models/fightModel");
+const deployFight = require("../scripts/03-fight");
 
-async function createChallenge(playerAddress, bidAmount) {
-  const newChallenge = new Challenge({ playerAddress, bidAmount });
+async function createChallenge(playerAddress, nftId, bidAmount) {
+  const newChallenge = new Challenge({ playerAddress, nftId, bidAmount });
   return await newChallenge.save();
 }
 
@@ -40,27 +41,61 @@ async function acceptSomeonesChallenge(
   nftId1,
   nftId2
 ) {
-  // Delete the challenge of Player 1
-  await Challenge.deleteOne({ playerAddress });
+  const MAX_BATTLES_ONLINE = 2;
+  const totalFightsCount = await Fight.countDocuments({ isActive: true });
+  // Check if servers can support more battles
+  if (totalFightsCount < MAX_BATTLES_ONLINE) {
+    // Delete the challenge of Player 1
+    await Challenge.deleteOne({ playerAddress });
 
-  // Create a new fight using the Fight model
-  const fightId = generateFightId(
-    playerAddress,
-    opponentAddress,
-    nftId1,
-    nftId2
-  );
-  const newFight = new Fight({
-    fightId,
-    player1: playerAddress,
-    player2: opponentAddress,
-    nftId1,
-    nftId2,
-    isActive: true,
-  });
+    // Notify the other user he has to start a fight.
+    // You would typically use a real-time messaging system such as websockets to accomplish this.
 
-  return await newFight.save();
+    // Backend notifies both users: Your fight is being deployed please wait.
+    // Again, you would typically use a real-time messaging system to accomplish this.
+
+    // Deploy fight (user sees message, deploying fight)
+    // This will likely involve some interaction with your smart contract.
+
+    // Send notification, enter fight page.
+    // Again, a real-time messaging system would be the way to go.
+
+    // If any user does not accept in 15 seconds, he loses. (backend calls fight contract as admin with the loser address,
+    // if both don't enter, he calls it with the challenger address)
+    // You might set a timer and have a separate function that checks the acceptance state in your database.
+
+    // Create a new fight using the Fight model
+    const fightId = generateFightId(
+      playerAddress,
+      opponentAddress,
+      nftId1,
+      nftId2
+    );
+    const newFight = new Fight({
+      fightId,
+      player1: playerAddress,
+      player2: opponentAddress,
+      nftId1,
+      nftId2,
+      isActive: true,
+    });
+
+    const fight = await newFight.save();
+
+    // After 15 seconds, check if users have accepted the fight
+    setTimeout(async () => {
+      const fightRefreshed = await Fight.findById(fight._id);
+      if (!fightRefreshed.player1Accepted || !fightRefreshed.player2Accepted) {
+        // End the fight in the blockchain and in your database
+      }
+    }, 15000);
+
+    return fight;
+  } else {
+    throw new Error("Fights servers are full now, please try later.");
+  }
 }
+
 
 function generateFightId(p1, p2, nftId1, nftId2) {
   return Buffer.from(p1 + p2 + nftId1.toString() + nftId2.toString()).toString(
